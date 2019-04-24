650d320239de35c3ac3a265495bf185ae812c498
dzw <1048938804@qq.com>
2019-04-24 20:45:55 +0800

    django项目(2)

--- blog/blog/templatetags/blog_tags.py
+++ blog/blog/templatetags/blog_tags.py
@@ -6,21 +6,25 @@ register= template.Library()
 
 
 #最新文章模板标签
+
 @register.simple_tag
 def get_recent_posts(num=5):
 
-    return Post.objects.all().order_by('_created_time')[:num]
+    return Post.objects.all().order_by('-created_time')[:num]
 
 
 #归档模板标签
+
 @register.simple_tag
 def archives():
     return Post.objects.dates('created_time','month',order='DESC')
 
 
 #分类模板标签
+
 @register.simple_tag
 def get_categories():
     return Category.objects.all()
+
 
 
