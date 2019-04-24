f332650f7b0bb8fc47fab3dfdc5d3640f8896bae
dzw <1048938804@qq.com>
2019-04-24 20:11:14 +0800

    django项目(1)

---
+++ blog/blog/templatetags/blog_tags.py
@@ -0,0 +1,26 @@
+from ..models import Post,Category
+from django import template
+
+#实例化template.Library类
+register= template.Library()
+
+
+#最新文章模板标签
+@register.simple_tag
+def get_recent_posts(num=5):
+
+    return Post.objects.all().order_by('_created_time')[:num]
+
+
+#归档模板标签
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time','month',order='DESC')
+
+
+#分类模板标签
+@register.simple_tag
+def get_categories():
+    return Category.objects.all()
+
+
